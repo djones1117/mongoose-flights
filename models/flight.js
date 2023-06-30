1ffcc497b14947f67aef1cd5af68886d3f8a84ac
djones1117 <socjones21@yahoo.com>
2023-06-30 16:43:09 -0400

    models/flights pt 1

--- models/flight.js
+++ models/flight.js
@@ -1,3 +1,24 @@
 const mongoose = require('mongoose');
 // optional shortcut to the mongoose.Schema class
 const Schema = mongoose.Schema;
+
+const flightSchema = new Schema({
+    airline: {
+        type: String,
+     enum: ['Delta', 'American', 'Southwest']
+    },
+    airport: {
+        type: String,
+      enum: ['LAX', 'SNA', 'DEN']  
+    },
+    flightNo: {
+        type: Number,
+        required: true,
+        min: 10,
+        max: 9999,
+     },
+    departs: {
+        type: Date,
+    }, 
+});
+
